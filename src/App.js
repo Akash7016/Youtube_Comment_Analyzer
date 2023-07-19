3929f9b6cf4b6c1ba430987f04af1f4197189490
Akash7016 <yadavakash9374@gmail.com>
2023-07-19 20:58:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,168 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { Box, Typography } from "@mui/material";
+import axios from "axios";
+import { Pie, Bar } from "react-chartjs-2";
+import Chart from "chart.js/auto";
+import { ArcElement } from "chart.js";
+import Header from "./component/Header";
+Chart.register(ArcElement);
 
-function App() {
+const CommentAnalyzer = () => {
+ 
+  const [videoLink, setVideoLink] = useState("");
+  const [comments, setComments] = useState(false);
+  const [positiveScore, setPositiveScore] = useState(0);
+  const [negativeScore, setNegativeScore] = useState(0);
+  const [neutralScore, setNeutralScore] = useState(0);
+
+  // Dictionary of words with values
+  const wordDictionary = {
+    awesome: 1,
+    great: 1,
+    love:1,
+    nice:1,
+    bad: -1,
+    useless: -1,
+    boring: -1
+    // Add more words and values as needed
+  };
+
+  function getVideoIdFromShortLink(shortLink) {
+    const regex = /youtu\.be\/([^?]+)/;
+    const match = shortLink.match(regex);
+    
+    if (match && match[1]) {
+      return match[1];
+    } else {
+      return null;
+    }
+  }
+  
+  
+  const videoId = getVideoIdFromShortLink(videoLink);
+  
+
+  // Function to fetch comments and analyze
+  const analyzeComments = async () => {
+    try {
+      const response = await axios.get(
+        `https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId=${videoId}&key=AIzaSyAy1ItksYzbvk5zgTt-1J7Csugij68tx6g`
+      );
+
+      const commentsData = response.data.items.map(
+        (item) => item.snippet.topLevelComment.snippet.textDisplay
+      );
+
+      // Perform comment analysis
+      let positiveCount = 0;
+      let negativeCount = 0;
+      let neutralCount = 0;
+
+      commentsData.forEach((comment) => {
+        const words = comment.toLowerCase().split(" ");
+
+        let commentScore = 0;
+        words.forEach((word) => {
+          if (wordDictionary[word]) {
+            commentScore += wordDictionary[word];
+          }
+        });
+
+        if (commentScore > 0) {
+          positiveCount++;
+        } else if (commentScore < 0) {
+          negativeCount++;
+        } else {
+          neutralCount++;
+        }
+      });
+
+      setComments(commentsData);
+      setPositiveScore(positiveCount);
+      setNegativeScore(negativeCount);
+      setNeutralScore(neutralCount);
+      setComments(true)
+    } catch (error) {
+      console.error("Error fetching comments:", error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <Box sx={{ height:'400px',display:"flex",justifyContent:'center',alignItems:'center', flexDirection:'column' }}>
+          <Box>
+            <Typography variant="h4" align="center" mt={2}>
+              Youtube Video Comment Analyzer
+            </Typography>
+            <Typography variant="subtitle1" align="center" mt={1} mb={5} sx={{ wordBreak: "break-word",width:'700px'}} >
+            An input a link to a YouTube video, a pie chart to display total likes and dislikes amounts, and a histogram to plot the results for different sentiment groups of comments.
+            </Typography>
+            
+          </Box>
+          <Box style={{display:'flex',justifyContent:'center',alignItems:'center',width:'900px'}}>
+              {/* Input field to enter YouTube video link */}
+              <TextField
+                id="outlined-basic"
+                label="Paste Youtube Video Link"
+                variant="outlined"
+                value={videoLink}
+                onChange={(e) => setVideoLink(e.target.value)}
+                fullWidth />
+              
+              <Button style={{marginLeft:'10px',padding:'4px'}} variant="contained" onClick={analyzeComments}>Analyze Comments</Button>
+              
+            </Box>
+      </Box>
+      {comments && 
+      <div style={{display:'flex',justifyContent:'space-evenly',alignItems:'center',}}>
+
+
+        {/* Pie chart to display sentiment breakdown */}
+        <div >
+
+        <Pie
+          data={{
+            labels: ["Positive", "Negative", "Neutral"],
+            datasets: [
+              {
+                data: [positiveScore, negativeScore, neutralScore],
+                backgroundColor: ["#36A2EB", "#FF6384", "#FFCE56"]
+              }
+            ]
+          }}
+        />
+        </div>
+
+        {/* Histogram to plot sentiment groups */}
+        <div >
+
+        <Bar height={300} width={300}
+          data={{
+            labels: ["Positive", "Negative", "Neutral"],
+            datasets: [
+              {
+                label: "Sentiment Groups",
+                data: [positiveScore, negativeScore, neutralScore],
+                backgroundColor: ["#36A2EB", "#FF6384", "#FFCE56"],
+                borderWidth: 1
+              }
+            ]
+          }}
+          options={{
+            indexAxis: "y"
+          }}
+        />
+        </div>
+      </div>
+      }
+
+
+      
     </div>
   );
-}
+};
 
-export default App;
+export default CommentAnalyzer;
---
+++ src/component/Header.js
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+
+
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+function Header() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'Montserrat',
+              fontWeight: 500,
+              color: 'inherit',
+              textDecoration: 'none',
+              fontSize:'18px'
+            }}
+          >
+            Youtube Comment Analyzer
+          </Typography>
+
+        
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default Header;
